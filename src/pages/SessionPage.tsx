05904b65125d06ee7def37ef6c253cf48f303d9f
andrewesiri-droid <andrewesiri@gmail.com>
2026-05-07 00:43:50 -0500

    fix: add useEffect import to SessionPage

--- src/pages/SessionPage.tsx
+++ src/pages/SessionPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { trpc } from '@/providers/trpc';
 import { useSessionData } from '@/hooks/useSessionData';
